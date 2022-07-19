d1aeec8a6ab62c0899e9a8c6adfd9733cfe8745a
acer-racer <102743232+acer-racer@users.noreply.github.com>
2022-07-19 22:01:02 +0200

    updates

--- LICENSE.md
+++ LICENSE.md
@@ -3,3 +3,6 @@ MIT License
 Packages used: 
 sdf-parser (https://github.com/cheminfo/sdf-parser) Copyright (c) 2014 cheminfo
 smiles-drawer (https://github.com/reymond-group/smilesDrawer) Copyright (c) 2017 GDB / Reymond Research Group
+
+APIs:
+Pubchem REST 
--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function search(){
     chem = document.getElementById('chemical').value;
     console.log(chem)
     const data_url="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"+chem+"/JSON";
-    getInchi(data_url);
+    getInchi(data_url,chem);
 }
 
-async function getInchi(data_url){
+async function getInchi(data_url,chem){
 
     searchResponse.innerHTML = "";
     structureData.innerHTML ="";
@@ -33,8 +33,9 @@ async function getInchi(data_url){
         searchResponse.setAttribute("style","color:red");
     }
     else{
+    
+
     searchResponse.setAttribute("style","color:black");
-    console.log(data);
     let arr=[];
     let dic=["IUPAC","InChI","MW","Formula","SMILES"];
     arr.push(data.PC_Compounds[0].props[9].value.sval);
@@ -43,6 +44,7 @@ async function getInchi(data_url){
     arr.push(data.PC_Compounds[0].props[16].value.sval);
     arr.push(data.PC_Compounds[0].props[18].value.sval);
 
+    getMolData(data.PC_Compounds[0].props[9].value.sval);
     draw(arr.slice(-1)[0]);
 
     const specs = document.createElement("ul");
@@ -57,3 +59,54 @@ async function getInchi(data_url){
     structureData.appendChild(specs);
 }};
 
+class molecule{
+    constructor(name) {
+        this.name = name;
+        this.atoms = new Map();
+        this.bonds = new Map();
+        this.atoms_ex_H = new Map();
+        this.bonds_ex_H = new Map();
+      }
+
+    get getTotalM(){
+        
+    } 
+}
+
+async function getMolData(IUPAC){  
+    url="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"+IUPAC+"/sdf";
+    const response = await fetch(url);
+    const data = await response.text();
+
+    const mol = new molecule(IUPAC);
+    let arr = data.split("\n");
+    let atom_count = parseInt(arr[3].substring(0,3));
+    let bonds_count = parseInt(arr[3].substring(3,6));
+
+    console.log(atom_count,bonds_count);
+
+    var c=1; var h=1;
+    for (let i=4;i<(4+atom_count);i++){
+        currAtom = arr[i].substring(31,33).trim();
+        mol.atoms[c]=currAtom;
+        if (currAtom !== "H"){
+            mol.atoms_ex_H[h]=currAtom;
+            h++;
+        }
+        c++;    
+    }
+    var c=1; var h=1;
+    for (let i=4+atom_count;i<(4+atom_count+bonds_count);i++){
+        atom1 = parseInt(arr[i].substring(0,3).trim());
+        atom2 = parseInt(arr[i].substring(3,6).trim());
+        type = parseInt(arr[i].substring(6,9).trim());
+        mol.bonds[c]={atom1,atom2,type};
+        if (mol.atoms[atom1]!=="H" && mol.atoms[atom2]!=="H"){
+            mol.bonds_ex_H[h]={atom1,atom2,type};
+            h++;
+        }
+        c++;   
+    }
+    console.log(mol.bonds,mol.atoms);
+    console.log(mol.bonds_ex_H,mol.atoms_ex_H);
+}
